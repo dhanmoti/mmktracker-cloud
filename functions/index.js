8956f28523e4a092b9ad28e58eb44b9b5be4ac02
Devi <dhanmoti@gmail.com>
2023-04-01 09:45:38 +0800

    added draft decorated json for mobile clients

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,24 @@ const functions = require("firebase-functions");
 const https = require("https");
 const Firestore = require("@google-cloud/firestore");
 const PROJECTID = "mmk-tracker-1d212";
-const COLLECTION_NAME = "latest";
+const COLLECTION_NAME = "central-bank-mm";
 
 const firestore = new Firestore({
   projectId: PROJECTID,
   timestampsInSnapshots: true,
 });
 
+
+exports.onUpdateTrigger = functions.firestore
+    .document("/central-bank-mm/latest-rates")
+    .onUpdate((change, eventContext) => {
+      // Data before update and after update
+      const newValue = change.after.data();
+      console.log(newValue);
+      updateDecoratedJson(newValue);
+      fetchCurrenciesCodeMap();
+      return null;
+    });
 
 exports.scheduledFetch = functions.pubsub.schedule("every 5 minutes")
     .onRun((context) => {
@@ -19,6 +30,7 @@ exports.scheduledFetch = functions.pubsub.schedule("every 5 minutes")
 
 /**
  * fetch MMK rates using central bank API
+ * https://forex.cbm.gov.mm/index.php/fxrate
  **/
 function fetch() {
   https.get("https://forex.cbm.gov.mm/api/latest", (res) => {
@@ -34,7 +46,7 @@ function fetch() {
       const rates = JSON.parse(Buffer.concat(data).toString());
       console.log("Rates");
       console.log(rates);
-      updateFirestoreCollection(rates);
+      updateLatestRates(rates);
     });
   }).on("error", (err) => {
     console.log("Error: ", err.message);
@@ -46,10 +58,63 @@ function fetch() {
  * Save to firestore collection
  * @Param {object} rates
  **/
-function updateFirestoreCollection(rates) {
+function updateLatestRates(rates) {
   firestore.collection(COLLECTION_NAME)
-      .doc("DUfouoDfgdutuOZs4opA")
-      .update({rates})
+      .doc("latest-rates")
+      .update(rates)
+      .then((doc) => {
+        console.info("stored new doc");
+      }).catch((err) => {
+        console.error(err);
+      });
+}
+
+/**
+ * Decorate json for mobile clients
+ * @Param {object} newRates
+ **/
+function updateDecoratedJson(newRates) {
+  firestore.collection(COLLECTION_NAME)
+      .doc("decorated-currencies-rates")
+      .update(newRates)
+      .then((doc) => {
+        console.info("stored new doc");
+      }).catch((err) => {
+        console.error(err);
+      });
+}
+
+/**
+ * fetch MMK rates using central bank API
+ * https://forex.cbm.gov.mm/index.php/fxrate
+ **/
+function fetchCurrenciesCodeMap() {
+  https.get("https://forex.cbm.gov.mm/api/currencies", (res) => {
+    const data = [];
+    console.log("Status Code:", res.statusCode);
+    res.on("data", (chunk) => {
+      data.push(chunk);
+    });
+
+    res.on("end", () => {
+      console.log("Response ended: ");
+      const currenciesMap = JSON.parse(Buffer.concat(data).toString());
+      updateFirestoreCollectionCurrenciesMap(currenciesMap["currencies"]);
+    });
+  }).on("error", (err) => {
+    console.log("Error: ", err.message);
+  });
+}
+
+
+/**
+ * Save to firestore collection
+ * @Param {object} map
+ **/
+function updateFirestoreCollectionCurrenciesMap(map) {
+  firestore.collection(COLLECTION_NAME)
+      .doc("currency-code-map")
+      .update(map)
       .then((doc) => {
         console.info("stored new doc");
       }).catch((err) => {
